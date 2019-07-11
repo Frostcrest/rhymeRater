7abce18822a8bed8a3de78607796f32172fbaa23
Frostcrest <jvaritek33c@gmail.com>
2019-07-11 18:04:35 -0400

    Complex rhyming comparison

--- lambda-service/main.py
+++ lambda-service/main.py
@@ -1,17 +1,12 @@
 import pronouncing
 
-def stringComparison():
+def linesInput():
 
     line1 = raw_input("Enter the first line. ")
     type(line1)
 
     line2 = raw_input("Enter the second line. ")
     type(line2)
-
-    if line1 == line2 :
-        print(line1 + ' is the same as ' + line2 + '. They match.')
-    else:
-        print(line1 + ' is not the same as ' + line2 + '. They do not match.')
 
     return line1, line2;
 
@@ -38,9 +33,52 @@ def simpleEndRhymeComparison(lines):
         print('This is not a perfect rhyme, please try again. Remember, a perfect rhyme in this program means the last two characters match.')
         stringComparison()
 
-if __name__ == "__main__":
-    pronouncing.rhymes("climbing")
-    lines = stringComparison()
-    print(lines)
 
-    simpleEndRhymeComparison(lines)
+def phoneticEndRhymeComparison(lines):
+
+    output = [pronouncing.rhymes(lines[0]),pronouncing.rhymes(lines[1])]
+    outputD = {0: pronouncing.rhymes(lines[0]), 1: pronouncing.rhymes(lines[1])}
+
+    # print(outputD)
+    # if outputD[0] != outputD[1]:
+    #     print('This has passed the complex rhyme test.')
+    # else:
+    #     print(outputD[0], " ", outputD[1])
+
+    # set(output[0]).intersection(output[1])
+    # set([5])
+
+    test_list1 = output[0]
+    test_list2 = output[1]
+
+    test_list1 = [str(r) for r in test_list1]
+    test_list2 = [str(r) for r in test_list2]
+    
+    # printing lists
+    print ("The first list is : " + str(test_list1))
+    print ("The second list is : " + str(test_list2))
+
+    # sorting both the lists
+    test_list1.sort()
+    test_list2.sort()
+
+    # using == to check if
+    # lists are equal
+    if test_list1 == test_list2:
+        print ("The lists are identical")
+    else :
+        print ("The lists are not identical")
+
+if __name__ == "__main__":
+
+    lines = linesInput()
+    phoneticEndRhymeComparison(lines)
+    # output1 = pronouncing.rhymes(lines[0])
+    # output2 = pronouncing.rhymes(lines[0])
+    #
+    # print(output1)
+    # print(output2)
+
+
+
+    phoneticEndRhymeComparison(lines)
